cc207e087596430d6cf1fb8ab3a092284a17f74e
schang7 <65791750+schang7@users.noreply.github.com>
2022-04-05 08:17:14 -0700

    updating some comments to the code

--- scripts/drive_square.py
+++ scripts/drive_square.py
@@ -23,32 +23,32 @@ class DriveSquare(object):
     def turn_in_square(self, event):
         # A callback function which makes the robot turn in the square, 
         # is called every time the robot traverses the length of one "side" 
-        # of the square in its driving path
+        # of the square once the driving course is initiated
 
-        # First stop the robot's forward motion if it is moving,
+        # First stop the robot's forward motion if it is going in that direction,
         # then initiate angular movement for turning to the right 
         self.movement.linear.x = 0
         self.movement.angular.z = 1.0
 
-        # Give it some time to register
+        # Give the command some time to register and then publish
         rospy.sleep(1)
         self.movement_pub.publish(self.movement)
 
-        # Determine duration needed to complete 90 degree turn
+        # Determine time duration needed to complete 90 degree turn
         radians = math.pi/2 # to make a 90 degrees turn (in radians)
-        duration_to_turn = radians / self.movement.angular.z
+        duration_to_turn = radians / self.movement.angular.z # in sec
 
         # Turn for that amount of time
         rospy.sleep(duration_to_turn)
 
-        # Stop moving in the angular direction, start moving forward again
+        # After the turn, stop moving in the angular direction and start moving forward again
         self.movement.angular.z = 0
         self.movement.linear.x = 0.5
         self.movement_pub.publish(self.movement)
 
     def run(self):
         # Set a timer to execute the turn_in_square callback function every time
-        # a certain amount of seconds have elapsed
+        # a specified amount of time have elapsed
         time_before_turn = 5 # in seconds
         rospy.Timer(rospy.Duration(time_before_turn), self.turn_in_square)
         rospy.spin()
