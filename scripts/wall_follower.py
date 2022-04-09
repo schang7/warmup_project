17e19192c914a4f7c4d76677b20c411fe78b36b9
schang7 <shchang2018@gmail.com>
2022-04-09 22:32:03 +0000

    fixed some of person_follower, need to add condition for wall_follower

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -15,16 +15,16 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 
 
-class PersonFollower(object):
-    """ This node finds a person and follows them around """
+class WallFollower(object):
+    """ This node finds a wall and follows them around """
 
     def __init__(self):
         # Initializing the ROS node
-        rospy.init_node("person_follower")
+        rospy.init_node("wall_follower")
 
         # Setting up the subscriber to the scan topic and
-        # putting self.follow_person as the callback function.
-        rospy.Subscriber("/scan", LaserScan, self.follow_person)
+        # putting self.follow_wall as the callback function.
+        rospy.Subscriber("/scan", LaserScan, self.follow_wall)
 
         # Setting up the publisher for the cmd_vel ROS topic
         self.movement_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
@@ -35,13 +35,13 @@ class PersonFollower(object):
             angular=Vector3()
         )
 
-        # A default distance that the robot should keep from the person
-        self.goal_following_dist = 0.3 # in meters
+        # A default distance that the robot should keep from the wall
+        self.goal_following_dist = 0.5 # in meters
 
-    def follow_person(self, data):
-        # Detect a person within the range of the robot using the scan data from
-        # everywhere around the robot, turn towards that detected individual, then 
-        # follow them around. The robot should also stop at a distance away from the person
+    def follow_wall(self, data):
+        # Detect a wall within the range of the robot using the scan data from
+        # everywhere around the robot, go towards that wall, then 
+        # follow it. The robot should follow at a distance away from the wall
         # so as not to get too close. 
 
         # CHANGE SO THAT IT's MORE ME
@@ -50,10 +50,6 @@ class PersonFollower(object):
         
         # ranges[0] corresponds with what is directly in front of the robot, and as index to ranges
         # increases it is a 
-
-        # IF PERSON IS DETECTED
-            # go towards the angle with a range that is greater than self.distance but
-            # has the minimum range ; in addition, it shouldn't be zero
         
         # Find all nonzero values
         ranges = np.asarray(data.ranges)
@@ -72,27 +68,18 @@ class PersonFollower(object):
             ranges[ranges == 0.0] = np.nan
             tile_up_by = int(20) # in degrees
             # goal angle is halfway in each pie slice
-            possible_goals = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, -160, -140, -120, -100, -80, -60, -40, -20]
+            possible_goals = [10, 30, 50, 70, 90, 110, 130, 150, 170, -170, -150, -130, -110, -90, -70, -50, -30, -10]
             #possible_goals = [0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,-170,-160,-150,-140,-130,-120,-110,-100,-90,-80,-70,-60,-50,-40,-30,-20,-10]
             slice_means = []
             num_slice_elements = []
 
             print(len(ranges))
             for s in range(int(len(ranges)/tile_up_by)):
-                if s == 0:
-                    first_half = slice(0,int(tile_up_by/2))
-                    second_half = slice(int(-tile_up_by/2),0)
-                    elements_in_slice = sum(nonzero_distances[:int(tile_up_by/2)]) + sum(nonzero_distances[-1*int(tile_up_by/2):])
+                offset = 0 
+                slice_idx = slice(int(offset + (s*tile_up_by)), int(offset + ((s+1)*tile_up_by)))
+                elements_in_slice = sum(nonzero_distances[slice_idx])
+                slice_mean = np.nanmean(ranges[slice_idx])
                     
-                    slice_mean = np.nanmean(np.append(ranges[:int((tile_up_by/2))],ranges[int((-tile_up_by/2)):]))
-                    
-                else:
-                    offset = tile_up_by/2
-                    slice_idx = slice(int(offset + ((s-1)*tile_up_by)), int(offset + (s*tile_up_by)))
-                    elements_in_slice = sum(nonzero_distances[slice_idx])
-                    slice_mean = np.nanmean(ranges[slice_idx])
-                    
-
                 num_slice_elements.append(elements_in_slice)
                 slice_means.append(slice_mean)
             print(num_slice_elements)
@@ -112,15 +99,8 @@ class PersonFollower(object):
             
             # this should be the mean for that given slice, but increase the k-value for the proportional control
             smallest_angle = possible_goals[slice_with_most_detection]
-            # 10 to -10
-            # -10 to -30, +10 to +30
-            # 30 to 50, 50 to 70, 70 to 90, 90 to 110, 110 to 130, 130 to 150, 150 to 170
-            # 170 to -170
-            # and so on until...
-            # 170 to 180, -170 to -180
-            # whichever has the greatest value, we set the goal angular velocity to the center
 
-            # Angle the robot in the direction of the closest object, still nonzero
+            # Angle the robot such that the 
             #smallest_distance = np.min(ranges[nonzero_distances])
             #smallest_angle = np.where(ranges == smallest_distance)[0][0]
             print('smallest angle: ' + str(smallest_angle))
@@ -130,20 +110,29 @@ class PersonFollower(object):
             # If it is within a range of values direction behind the robot (+/- 5 degrees from 180 degrees), 
             # we define that it will rotate left until it reaches that destination 
             # (to reduce the chance of noise impacting the angular movement decision)
-            angle_error = smallest_angle
+            angle_error = smallest_angle - 90
 
             angle_k = 0.05
-                        
-            self.movement.angular.z = angle_k * angle_error
+
+            # we've run into a wall and are following it now
+
+            if smallest_distance < self.goal_following_dist: 
+                print("went into smallest_dist")
+                self.movement.angular.z = angle_k * angle_error
+             
+            # we're either away from a wall or pulling away from it entirely
+            else:
+                self.movement.angular.z = 0 
+
             print("angular_accel: " + str(self.movement.angular.z)) 
             dist_error = smallest_distance - self.goal_following_dist
-            print("smallest_dist: " + str(smallest_distance))
-            print("error: " + str(dist_error))
-            dist_k = 0.75
-            self.movement.linear.x = dist_k * dist_error
+            #print("smallest_dist: " + str(smallest_distance))
+            #print("dist_error: " + str(dist_error))
+            dist_k = 0.5
+            #self.movement.linear.x = 0.1
+            self.movement.linear.x = 0.1
             print("linear move: " + str(self.movement.linear.x)) 
         self.movement_pub.publish(self.movement)
-
 
     def run(self):
         # Keep the program alive.
@@ -151,5 +140,10 @@ class PersonFollower(object):
 
 if __name__ == '__main__':
     # Declare a node and run it.
-    node = PersonFollower()
+    node = WallFollower()
     node.run()
+
+
+
+
+
